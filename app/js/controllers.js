d83dc403dc2c5f120b59f8b37ba3762befb27c17
Melissa Ip <nokami.hybrids@gmail.com>
2014-05-31 01:38:22 -0700

    auto set priority and overdue color

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,37 +5,75 @@
 angular.module('myApp.controllers', ['myApp.services'])
 	.controller('TaskController', ['$scope', function($scope) {
       $scope.tasks = [
-        { date: '06/01/2014', name: 'Do laundry', description: 'Whites only',
-          priority: 'high', isDone: false, isVisible: true },
-        { date: '06/02/2014', name: 'Wash dishes', description: 'Handwash pots',
-          priority: 'medium', isDone: false, isVisible: true },
-        { date: '06/03/2014', name: 'Make bed', description: 'By 10am',
-          priority: 'low', isDone: false, isVisible: true }
+        { date: moment('06/01/2014', 'MM/DD/YYYY'), name: 'Do laundry',
+          description: 'Whites only', priority: 'high',
+          isDone: false, isVisible: true },
+        { date: moment('06/02/2014', 'MM/DD/YYYY'), name: 'Wash dishes',
+          description: 'Handwash pots', priority: 'medium',
+          isDone: false, isVisible: true },
+        { date: moment('06/03/2014', 'MM/DD/YYYY'), name: 'Make bed',
+          description: 'By 10am', priority: 'low',
+          isDone: false, isVisible: true }
       ];
       $scope.currentDate = '';
       $scope.currentName = '';
       $scope.currentDescription = '';
-      $scope.currentPriority = 'high';
-      $scope.priorities = ['high', 'medium', 'low'];
+      $scope.currentPriority = 'auto';
+      $scope.priorities = ['auto', 'overdue', 'high', 'medium', 'low'];
 
       $scope.setColor = function(task) {
         if (task.isDone) {
           return { color: 'lime' };
+        } else if (task.priority === 'overdue') {
+          return { color: 'red' };
         } else if (task.priority === 'high') {
-      		return { color: 'red' };
+      		return { color: 'maroon' };
       	} else if (task.priority === 'medium') {
       		return { color: 'orange' };
         } else if (task.priority === 'low') {
           return { color: 'green' };
       	}
       };
-      
+
+      $scope.setPriority = function(task, p) {
+        if (p === 'auto') {
+          var diff = task.date.diff(moment(), 'days');
+          if (diff == 0 || diff == 1) {
+            task.priority = 'high';
+          } else if (diff > 1 && diff <= 7) {
+            task.priority = 'medium';
+          } else if (diff > 7) {
+            task.priority = 'low';
+          } else {
+            task.priority = 'overdue';
+          }
+        }
+      };
+
+      $scope.getDate = function (task) {
+        return task.date.format('MM/DD/YYYY');
+      };
+
       $scope.add = function() {
+        var parsedDate = moment($scope.currentDate, 'MM/DD/YYYY');
+        var priorityAuto = $scope.currentPriority;
+        if ($scope.currentPriority === 'auto') {
+          var diff = parsedDate.diff(moment(), 'days');
+          if (diff == 0 || diff == 1) {
+            priorityAuto = 'high';
+          } else if (diff > 1 && diff <= 7) {
+            priorityAuto = 'medium';
+          } else if (diff > 7) {
+            priorityAuto = 'low';
+          } else {
+            priorityAuto = 'overdue';
+          }
+        }
       	$scope.tasks.unshift({
-      		date: $scope.currentDate,
+      		date: parsedDate,
           name: $scope.currentName,
           description: $scope.currentDescription,
-      		priority: $scope.currentPriority,
+      		priority: priorityAuto,
           isDone: false,
           isVisible: true
       	});
